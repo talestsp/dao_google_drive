b1b82507d5852e241fc26281889f5930232de408
Tales Tenorio Pimentel <talestsp@users.noreply.github.com>
2021-05-19 15:12:43 -0300

    Function to create temp data dir

--- src/dao/dao_google_drive.py
+++ src/dao/dao_google_drive.py
@@ -14,9 +14,9 @@ drive = GoogleDrive(gauth)
 DATA_DIR = "data/"
 TEMP_DATA_DIR = DATA_DIR + "temp/"
 
-def load_data(dataset_shareable_link):
+def load_data(dataset_shareable_link, sep):
     google_drive_file_id = get_google_drive_file_id_from_shareable_link(dataset_shareable_link)
-    data = load_csv_from_google_drive_file_id(google_drive_file_id)
+    data = load_csv_from_google_drive_file_id(google_drive_file_id, sep)
     return data
 
 def get_google_drive_file_id_from_shareable_link(shareable_link):
@@ -26,12 +26,19 @@ def remove_file(filepath):
     if os.path.exists(filepath):
         os.remove(filepath)
 
-def load_csv_from_google_drive_file_id(google_drive_file_id):
+def create_dir(dirpath):
+    if not os.path.exists(dirpath):
+        os.mkdir(dirpath)
+
+def load_csv_from_google_drive_file_id(google_drive_file_id, sep):
     csv_filepath = TEMP_DATA_DIR + '_temp.csv'
+    create_dir(DATA_DIR)
+    create_dir(TEMP_DATA_DIR)
 
     downloaded = drive.CreateFile({'id' : google_drive_file_id})
     downloaded.GetContentFile(csv_filepath)
-    csv_file = pd.read_csv(csv_filepath)
+    csv_file = pd.read_csv(csv_filepath, sep=sep)
     remove_file(csv_filepath)
     return csv_file
+
 
